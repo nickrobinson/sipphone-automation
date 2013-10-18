e547d9e3107edb8b489ad71b4fcc1ad9870e8d3d
Nick Robinson <nr33@msstate.edu>
2013-10-17 20:06:40 -0500

    Adding intial contentsof keywords.py

---
+++ src/SipPhoneLibrary/keywords.py
@@ -0,0 +1,15 @@
+import robot
+
+from robot.libraries.BuiltIn import BuiltIn
+
+class PhoneKeywords(object):
+	ROBOT_LIBRARY_SCOPE = 'Global'
+
+	def __init__(self):
+		self.phones = {}
+		self.builtin = BuiltIn()
+
+	def setup_phone(extension, ipaddr, username, password):
+		self.phones[extension] = (ipaddr, username, password)
+
+	
