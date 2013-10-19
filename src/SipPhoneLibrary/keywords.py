166396cd070229a8cdf10b238d9c48db0042add6
Nick Robinson <nr33@msstate.edu>
2013-10-19 17:59:23 -0500

    Updating helper function that sends out requests

--- src/SipPhoneLibrary/keywords.py
+++ src/SipPhoneLibrary/keywords.py
@@ -14,8 +14,12 @@ class PhoneKeywords(object):
 		self.phones = {}
 		self.builtin = BuiltIn()
 
-	def _send_request(self, ipaddr, request):
+	def _send_request(self, extension, request):
 		headers = { 'Content-Type' : 'application/x-com-polycom-spipx' }
+		URL = "http://" + self.phones[extension][0] + "/push"
+		data = json.dumps(request)
+		auth = digest(self.phones[extension][1], self.phones[extension][2])
+		result=requests.post(URL, auth, verify=False, data, headers)
 
 	def setup_phone(self, extension, ipaddr, username, password):
 		self.phones[extension] = (ipaddr, username, password)
@@ -23,23 +27,24 @@ class PhoneKeywords(object):
 
 	def call_number(self, extension, number):
 		URL = BEGIN_REQUEST + "tel:\\" + number + END_REQUEST
-		self._send_request(self.phones[extension][0], URL)
+		self._send_request(extension, URL)
 
 	def max_volume(self, extension):
-		URL = BEGIN_REQUEST + "Key:VolUp" + END_REQUEST
-		self._send_request(self.phones[extension][0], URL)	
+		for i in range(10):
+			URL = BEGIN_REQUEST + "Key:VolUp" + END_REQUEST
+			self._send_request(extension, URL)	
 
 	def press_headset_key(self, extension):
 		URL = BEGIN_REQUEST + "Key:Headset" + END_REQUEST
-		self._send_request(self.phones[extension][0], URL)
+		self._send_request(extension, URL)
 
 	def press_end_call(self, extension):
 		URL = BEGIN_REQUEST + "Key:Softkey2" + END_REQUEST
-		self._send_request(self.phones[extension][0], URL)
+		self._send_request(extension, URL)
 
 	def press_dnd(self, extension):
 		URL = BEGIN_REQUEST + "Key:DoNotDisturb" + END_REQUEST
-		self._send_request(self.phones[extension][0], URL)
+		self._send_request(extension, URL)
 
 	def press_digit(self, extension, digit):
 		if digit == "*":
@@ -48,17 +53,22 @@ class PhoneKeywords(object):
 			digit = "Pound"
 
 		URL = BEGIN_REQUEST + "Key:DialPad" + digit + END_REQUEST
-		self._send_request(self.phones[extension][0], URL)
+		self._send_request(extension, URL)
 
 	def press_hold(self, extension):
 		URL = BEGIN_REQUEST + "Key:Hold" + END_REQUEST
-		self._send_request(self.phones[extension][0], URL)
+		self._send_request(extension, URL)
 
 	def mute_mic(self, extension):
 		URL = BEGIN_REQUEST + "Key:MicMute" + END_REQUEST
-		self._send_request(self.phones[extension][0], URL)
+		self._send_request(extension, URL)
 
 	def press_line_key(self, extension, lineNumber):
 		URL = BEGIN_REQUEST + "Key:Line" + lineNumber + END_REQUEST
-		self._send_request(self.phones[extension][0], URL)
+		self._send_request(extension, URL)
+
+	def press_transfer(self, extension):
+		URL = BEGIN_REQUEST + "Key:Transfer" + lineNumber + END_REQUEST
+		self._send_request(extension, URL)
+
 			
